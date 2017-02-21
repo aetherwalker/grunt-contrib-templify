0a728f59c51c1bcc3c0ede36f30014aea827febd
Alexander Anderson <anderson.1368@osu.edu>
2017-02-21 18:06:43 -0500

    Added additional unit test specifications
    Unit tests not yet flushed out

---
+++ specs/mode-generic/generic-spec.js
@@ -0,0 +1,13 @@
+
+describe("Karma template creation mode", function() {
+	
+	var options, sourceDir, file;
+	
+	beforeEach(function() {
+		
+	});
+	
+	it("Creates the specified output file", function() {
+		
+	});
+});

80665f630f652f5da9e6cc948f04377d84780f4f
aetherwalker <aetherwalker@refugesystems.net>
2018-05-19 22:15:13 -0400

    Added a valid var declaration to top level reference to "Templify"

--- builders/vue.js
+++ builders/vue.js
@@ -1,21 +1,21 @@
 module.exports = {
 	"mode": "vue",
 	"prefix": function() {
-		return "Templify = {};\n" +
-				"Templify.install = function(Vue, options) {\n" +
-				"\toptions = options || {};\n" +
-				"\toptions.name = options.name || \"templified\";\n" +
-				"\tVue[options.name] = function(name) {\n" +
-				"\t\tswitch(name) {\n";
+		return "var Templify = {};\n" +
+		"Templify.install = function(Vue, options) {\n" +
+		"\toptions = options || {};\n" +
+		"\toptions.name = options.name || \"templified\";\n" +
+		"\tVue[options.name] = function(name) {\n" +
+		"\t\tswitch(name) {\n";
 	},
 	"generator": function(template) {
 		return "\t\t\tcase \"" + template.name + "\": return " + JSON.stringify(template.html) + ";\n";
 	},
 	"suffix": function() {
 		return "\t\t\tdefault: return null;\n" +
-				"\t\t}\n" +
-				"\t};\n" +
-				"};\n" +
-				"Vue.use(Templify);";
+		"\t\t}\n" +
+		"\t};\n" +
+		"};\n" +
+		"Vue.use(Templify);";
 	}
 };
